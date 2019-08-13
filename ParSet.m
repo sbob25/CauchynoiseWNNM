c917fa4d670ac176757994cd26a44b9b89fe89ba
sbob25 <38348065+sbob25@users.noreply.github.com>
2019-08-13 15:27:52 +0900

    Add files via upload

--- ParSet.m
+++ ParSet.m
@@ -14,4 +14,4 @@ Par.patsize   = 6;                 % Patch size
 Par.patnum    = 70;                % Initial Non-local Patch number
 Par.iter      = 2;
 Par.eta       = 0.6;               % Noise estimete parameter
-Par.step      = floor((Par.patsize)/2-1);
+Par.step      = 2;
